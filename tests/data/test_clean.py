8379b0554651fc050017a6753778ad4accba2134
AchilleSalaun <achillesalaun@gmail.com>
2022-07-16 15:05:01 +0100

    dissociate tests for clean_data and wrangle_data

--- tests/data/test_clean.py
+++ tests/data/test_clean.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-from project.data.clean    import clean_data
+from project.data.clean import *
 from project.data.describe import Entry, Descriptor
+from project.misc.dataframes import build_empty_mask
 
 DESCRIPTOR = Descriptor(
     [
@@ -12,73 +13,77 @@ DESCRIPTOR = Descriptor(
 )
 
 DF1 = pd.DataFrame({'uppER': [1, 2], 'LoWEr': [3, 4.5]})
-DF2 = pd.DataFrame({'col1' : [1, 2], 'col2' : [1, 2]})
+DF2 = pd.DataFrame({'col1': [1, 2], 'col2': [1, 2]})
 DF3 = pd.DataFrame({'A': [1, 0, pd.NA], 'B': [1, pd.NA, pd.NA], 'C': [pd.NA, pd.NA, pd.NA]})
 
+
 def test_clean_data_columns_to_lower_case():
-    assert (clean_data(DF1).columns == ['upper', 'lower']).all()
-    
+    assert (set_columns_to_lower_case(DF1).columns == ['upper', 'lower']).all()
+
+
 def test_clean_data_int64():
     L = ['Int64', 'float64']
-    L_  = clean_data(DF1).dtypes.to_list()
+    L_ = change_int64(DF1).dtypes.to_list()
 
     assert len(L) == len(L_)
     for l, l_ in zip(L, L_):
         assert l == l_
 
+
 def test_clean_data_type_uniformity():
-    DF   = pd.DataFrame({
-            'col1': pd.Series([1, 2], dtype="Int64"),
-            'col2': pd.Series(["1", "2"], dtype="object")
-        })
+    DF = pd.DataFrame({
+        'col1': pd.Series([1, 2]),
+        'col2': pd.Series(["1", "2"], dtype="object")
+    })
 
-    hc  = {"col2", "col3"}
-    DF_ = clean_data(DF2, heterogeneous_columns=hc)
-    
-    assert (DF_.equals(DF))
-    
+    hc = {"col2", "col3"}
+    DF_ = ensure_type_uniformity(DF2.copy(), heterogeneous_columns=hc)
+
+    assert (DF_.dtypes == DF.dtypes).all()
+
+
 def test_clean_data_type_unknown_values():
-    DF  = pd.DataFrame({'col1': [pd.NA, pd.NA], 'col2': [1, pd.NA]}, dtype="Int64")
+    mask = pd.DataFrame({'col1': [True, True], 'col2': [False, True]})
 
-    gu  = {2}
-    su  = {'col1' : {1}}
-    DF_ = clean_data(DF2, generic_unknowns=gu, specific_unknowns=su)
-    
-    assert DF.equals(DF_)
+    gu = {2}
+    su = {'col1': {1}}
+    _, mask_ = correct_unknown_values(DF2.copy(), build_empty_mask(DF2), generic_unknowns=gu, specific_unknowns=su)
+
+    assert mask.equals(mask_)
+
 
 def test_clean_data_abnormal_values():
     limits = [({'col1'}, (0, 1.5))]
-    
-    DF   = pd.DataFrame({
-            'col1': pd.Series([1, pd.NA], dtype="Int64"), 
-            'col2': pd.Series([1, 2], dtype="Int64")
-        })
 
-    DF_  = clean_data(DF2, limits=limits)
-    assert (DF_.equals(DF))
+    mask = pd.DataFrame({'col1': [False, True], 'col2': [False, False]})
+
+    _, mask_ = remove_abnormal_values(DF2.copy(), build_empty_mask(DF2), limits=limits)
+
+    assert (mask_.equals(mask))
+
 
 def test_clean_data_categories():
     refs = [
         (
             {'col1'},
             {
-                1 : 'A',
-                2 : 'B',
-                3 : 'C'
+                1: 'A',
+                2: 'B',
+                3: 'C'
             }),
         (
             {'col2'},
             {
-                1 : 'D'
+                1: 'D'
             }
         )
     ]
-    
-    DF   = pd.DataFrame({
-            'col1': ['A', 'B'], 
-            'col2': ['D',  2 ]
-        })
 
-    DF_  = clean_data(DF2, references=refs)
+    DF = pd.DataFrame({
+        'col1': ['A', 'B'],
+        'col2': ['D', 2]
+    })
+
+    DF_ = use_category_names(DF2.copy(), references=refs)
     assert (DF_.equals(DF))
-    
+
--- tests/data/test_wrangle.py
+++ tests/data/test_wrangle.py
@@ -1,16 +1,9 @@
-import numpy  as np
+import numpy as np
 import pandas as pd
 
-from project.data.describe      import Entry, Descriptor
-from project.data.wrangle       import build_min,              \
-                                       build_max,              \
-                                       build_easy_trend,       \
-                                       build_binary_code,      \
-                                       get_constant_columns,   \
-                                       get_irrelevant_columns, \
-                                       get_sparse_columns,     \
-                                       wrangle_data
-from project.misc.clinical      import compute_egfr
+from project.data.describe import Entry, Descriptor
+from project.data.wrangle import *
+from project.misc.clinical import compute_egfr
 from project.misc.miscellaneous import identity
 
 DF1 = pd.DataFrame(
@@ -22,8 +15,8 @@ DF1 = pd.DataFrame(
 DF2 = pd.DataFrame({'A': [1, 0, pd.NA], 'B': [1, pd.NA, pd.NA], 'C': [pd.NA, pd.NA, pd.NA]})
 DF3 = pd.DataFrame(
     {
-        'A': [pd.NA,    -2,     5, pd.NA, -1, 42, pd.NA, -8, 0],
-        'B': [pd.NA, pd.NA, pd.NA,     1,  1,  1,     0,  0, 0],
+        'A': [pd.NA, -2, 5, pd.NA, -1, 42, pd.NA, -8, 0],
+        'B': [pd.NA, pd.NA, pd.NA, 1, 1, 1, 0, 0, 0],
         'C': np.random.randn(9),
     })
 
@@ -35,6 +28,7 @@ DESCRIPTOR1 = Descriptor(
     ]
 )
 
+
 ###############################
 #      BUILD NEW COLUMNS      #
 ###############################
@@ -43,6 +37,7 @@ DESCRIPTOR1 = Descriptor(
 def test_build_easy_trend():
     s = build_easy_trend(DF1, ['col_10', 'col_20'])
     assert s.equals(pd.Series([1, -1, 0]))
+
 
 def test_build_min():
     s = build_min(DF1, ['col_10', 'col_20'])
@@ -90,55 +85,55 @@ def test_get_sparse_columns():
 #####################
 
 DF4 = pd.DataFrame({
-    'irrelevant'      : [53147 , 36278 , 14526 ],
-    'constant1'       : [11111 , 11111 , 11111 ],
-    'constant2'       : [11111 , pd.NA , 11111 ],
-    'constant3'       : [pd.NA , pd.NA , pd.NA ],
-    'dage'            : [18.0  , 18.0  , 18.0  ],
-    'dsex'            : ["Male", "Male", "Male"],
-    'dweight'         : [0.0   , 1.0   , 100.0 ],
-    'dheight'         : [1.0   , 100.0 , 200.0 ],
-    'dbmi'            : [np.nan, np.nan, 25.0  ],
-    'rage'            : [18.0  , 18.0  , 18.0  ],
-    'rsex'            : ["Male", "Male", "Male"],
-    'rweight'         : [1.0   , 100.0 , -1.0  ],
-    'rheight'         : [100.0   , 200.0 , 1.0   ],
-    'rbmi'            : [np.nan, np.nan, np.nan],
-    'dial_at_reg'     : [pd.NA , "Not on dialysis", "B"  ],
-    'dial_at_tx'      : [pd.NA , "No"             , "Yes"],
-    'dial_at_tx_type' : [pd.NA , "Not on dialysis", "A"  ],
-    'days_on_dial_tx' : [np.nan, np.nan           , 42.0 ],
-    'creatinine_11'   : [np.nan, np.nan, 90.0  ],
-    'degfr_11'        : [42.0  , np.nan, 100.0 ],
-    'alt_11'          : [np.nan, 1.0   , 2.0   ],
-    'alt_12'          : [1.0   , 2.0   , 0.0   ],
-    'ast_11'          : [1.0   , 2.0   , np.nan],
-    'ast_12'          : [1.0   , 1.0   , 5.0   ],
-    'ast_81'          : [2.0   , np.nan, 0.0   ],
-    'amylase_11'      : [1.0   , 1.0   , 1.0   ],
-    'amylase_81'      : [0.0   , 5.0   , 1.0   ]
+    'irrelevant': [53147, 36278, 14526],
+    'constant1': [11111, 11111, 11111],
+    'constant2': [11111, pd.NA, 11111],
+    'constant3': [pd.NA, pd.NA, pd.NA],
+    'dage': [18.0, 18.0, 18.0],
+    'dsex': ["Male", "Male", "Male"],
+    'dweight': [0.0, 1.0, 100.0],
+    'dheight': [1.0, 100.0, 200.0],
+    'dbmi': [np.nan, np.nan, 25.0],
+    'rage': [18.0, 18.0, 18.0],
+    'rsex': ["Male", "Male", "Male"],
+    'rweight': [1.0, 100.0, -1.0],
+    'rheight': [100.0, 200.0, 1.0],
+    'rbmi': [np.nan, np.nan, np.nan],
+    'dial_at_reg': [pd.NA, "Not on dialysis", "B"],
+    'dial_at_tx': [pd.NA, "No", "Yes"],
+    'dial_at_tx_type': [pd.NA, "Not on dialysis", "A"],
+    'days_on_dial_tx': [np.nan, np.nan, 42.0],
+    'creatinine_11': [np.nan, np.nan, 90.0],
+    'degfr_11': [42.0, np.nan, 100.0],
+    'alt_11': [np.nan, 1.0, 2.0],
+    'alt_12': [1.0, 2.0, 0.0],
+    'ast_11': [1.0, 2.0, np.nan],
+    'ast_12': [1.0, 1.0, 5.0],
+    'ast_81': [2.0, np.nan, 0.0],
+    'amylase_11': [1.0, 1.0, 1.0],
+    'amylase_81': [0.0, 5.0, 1.0]
 })
 
-DF4_ = pd.DataFrame({
-    'dweight'         : [np.nan, 1.0   , 100.0 ],
-    'dheight'         : [np.nan, 100.0 , 200.0 ],
-    'dbmi'            : [np.nan, 1.0   , 25.0  ],
-    'rweight'         : [1.0   , 100.0 , np.nan],
-    'rheight'         : [100.0 , 200.0 , np.nan],
-    'rbmi'            : [1.0   , 25    , np.nan],
-    'dial_type'       : [pd.NA , "Not on dialysis", "A" ],
-    'dial_days'       : [np.nan, 0.0              , 42.0],
-    'alt_trend'       : [0     , 1     , -1    ],
-    'alt_min'         : [1.0   , 1.0   , 0.0   ],
-    'alt_max'         : [1.0   , 2.0   , 2.0   ],
-    'ast_trend'       : [0     , -1    , 0     ],
-    'ast_min'         : [1.0   , 1.0   , 0.0   ],
-    'ast_max'         : [2.0   , 2.0   , 5.0   ],
-    'amylase_min'     : [0.0   , 1.0   , 1.0   ],
-    'amylase_max'     : [1.0   , 5.0   , 1.0   ],
-    'degfr_min'       : [42.0  , np.nan, compute_egfr(18, 90, "Male", 200)],
-    'degfr_max'       : [42.0  , np.nan, compute_egfr(18, 90, "Male", 200)]
-})
+# DF4_ = pd.DataFrame({
+#     'dweight': [np.nan, 1.0, 100.0],
+#     'dheight': [np.nan, 100.0, 200.0],
+#     'dbmi': [np.nan, 1.0, 25.0],
+#     'rweight': [1.0, 100.0, np.nan],
+#     'rheight': [100.0, 200.0, np.nan],
+#     'rbmi': [1.0, 25, np.nan],
+#     'dial_type': [pd.NA, "Not on dialysis", "A"],
+#     'dial_days': [np.nan, 0.0, 42.0],
+#     'alt_trend': [0, 1, -1],
+#     'alt_min': [1.0, 1.0, 0.0],
+#     'alt_max': [1.0, 2.0, 2.0],
+#     'ast_trend': [0, -1, 0],
+#     'ast_min': [1.0, 1.0, 0.0],
+#     'ast_max': [2.0, 2.0, 5.0],
+#     'amylase_min': [0.0, 1.0, 1.0],
+#     'amylase_max': [1.0, 5.0, 1.0],
+#     'degfr_min': [42.0, np.nan, compute_egfr(18, 90, "Male", 200)],
+#     'degfr_max': [42.0, np.nan, compute_egfr(18, 90, "Male", 200)]
+# })
 
 DESCRIPTOR2 = Descriptor(
     [
@@ -150,5 +145,143 @@ DESCRIPTOR2.set_entry(Entry("irrelevant", tags="irrelevant"))
 DESCRIPTOR2.set_entry(Entry("dial_at_tx", tags="feature", binary_keys={"Yes": 0, "No": 1}))
 
 
-def test_wrangle_data():
-    assert wrangle_data(DF4, DESCRIPTOR2, limits_bmi=(0, 100)).equals(DF4_)
+def test_wrangle_data_impute_bmi():
+    input_df = pd.DataFrame({
+        'dweight': [0.0, 1.0, 100.0],
+        'dheight': [1.0, 100.0, 200.0],
+        'dbmi': [np.nan, np.nan, 25.0],
+        'rweight': [1.0, 100.0, -1.0],
+        'rheight': [100.0, 200.0, 1.0],
+        'rbmi': [np.nan, np.nan, np.nan]
+    })
+
+    target_df = pd.DataFrame({
+        'dweight': [np.nan, 1.0, 100.0],
+        'dheight': [np.nan, 100.0, 200.0],
+        'dbmi': [np.nan, 1.0, 25.0],
+        'rweight': [1.0, 100.0, np.nan],
+        'rheight': [100.0, 200.0, np.nan],
+        'rbmi': [1.0, 25, np.nan]
+    })
+
+    output_df = impute_bmi(input_df, limits_bmi=(0, 100))
+
+    assert target_df.equals(output_df)
+
+
+def test_wrangle_data_transform_dialysis_columns():
+    input_df = pd.DataFrame({
+        'dial_at_reg': [pd.NA, "Not on dialysis", "B"],
+        'dial_at_tx': [pd.NA, "No", "Yes"],
+        'dial_at_tx_type': [pd.NA, "Not on dialysis", "A"],
+        'days_on_dial_tx': [np.nan, np.nan, 42.0]
+    })
+
+    target_df = pd.DataFrame({
+        'dial_type': [pd.NA, "Not on dialysis", "A"],
+        'dial_days': [np.nan, 0.0, 42.0]
+    })
+
+    output_df = transform_dialysis_columns(input_df, DESCRIPTOR2)
+
+    assert target_df.equals(output_df)
+
+
+def test_wrangle_data_recompute_egfr():
+    input_df = pd.DataFrame({
+        'dsex': ["Male", "Male", "Male"],
+        'dheight': [np.nan, 100.0, 200.0],
+        'dage': [18.0, 18.0, 18.0],
+        'creatinine_11': [np.nan, np.nan, 90.0],
+        'degfr_11': [42.0, np.nan, 100.0]
+    })
+
+    target_df = pd.DataFrame({
+        'dsex': ["Male", "Male", "Male"],
+        'dheight': [np.nan, 100.0, 200.0],
+        'dage': [18.0, 18.0, 18.0],
+        'creatinine_11': [np.nan, np.nan, 90.0],
+        'degfr_11': [42.0, np.nan, compute_egfr(18, 90, "Male", 200)]
+    })
+
+    output_df = recompute_egfr(input_df)
+
+    assert target_df.equals(output_df)
+
+
+def test_wrangle_data_impute_biolevels():
+    input_df = pd.DataFrame({
+        'creatinine_11': [np.nan, np.nan, 90.0],
+        'degfr_11': [42.0, np.nan, 100.0],
+        'alt_11': [np.nan, 1.0, 2.0],
+        'alt_12': [1.0, 2.0, 0.0],
+        'ast_11': [1.0, 2.0, np.nan],
+        'ast_12': [1.0, 1.0, 5.0],
+        'ast_81': [2.0, np.nan, 0.0],
+        'amylase_11': [1.0, 1.0, 1.0],
+        'amylase_81': [0.0, 5.0, 1.0]
+    })
+
+    target_df = pd.DataFrame({
+        'alt_trend': [0, 1, -1],
+        'alt_min': [1.0, 1.0, 0.0],
+        'alt_max': [1.0, 2.0, 2.0],
+        'ast_trend': [0, -1, 0],
+        'ast_min': [1.0, 1.0, 0.0],
+        'ast_max': [2.0, 2.0, 5.0],
+        'amylase_trend': [0, 0, 0],
+        'amylase_min': [0.0, 1.0, 1.0],
+        'amylase_max': [1.0, 5.0, 1.0],
+        'creatinine_trend': [np.nan, np.nan, 0],
+        'creatinine_min': [np.nan, np.nan, 90.0],
+        'creatinine_max': [np.nan, np.nan, 90.0],
+        'degfr_trend': [0, np.nan, 0],
+        'degfr_min': [42.0, np.nan, 100.0],
+        'degfr_max': [42.0, np.nan, 100.0]
+    })
+
+    output_df = impute_biolevels(input_df)
+
+    assert target_df.equals(output_df)
+
+
+def test_wrangle_data_remove_irrelevant_columns():
+    input_df = pd.DataFrame({
+        'irrelevant': [53147, 36278, 14526],
+        'dage': [18.0, 18.0, 18.0],
+        'dsex': ["Male", "Male", "Male"],
+        'dweight': [0.0, 1.0, 100.0],
+        'dheight': [1.0, 100.0, 200.0],
+        'dbmi': [np.nan, np.nan, 25.0],
+    })
+
+    target_df = pd.DataFrame({
+        'dage': [18.0, 18.0, 18.0],
+        'dsex': ["Male", "Male", "Male"],
+        'dweight': [0.0, 1.0, 100.0],
+        'dheight': [1.0, 100.0, 200.0],
+        'dbmi': [np.nan, np.nan, 25.0],
+    })
+
+    output_df = remove_irrelevant_columns(input_df, DESCRIPTOR2)
+
+    assert target_df.equals(output_df)
+
+
+def test_wrangle_data_remove_constant_columns():
+    input_df = pd.DataFrame({
+        'constant1': [11111, 11111, 11111],
+        'constant2': [11111, pd.NA, 11111],
+        'constant3': [pd.NA, pd.NA, pd.NA],
+        'dweight': [0.0, 1.0, 100.0],
+        'dheight': [1.0, 100.0, 200.0],
+    })
+
+    target_df = pd.DataFrame({
+        'dweight': [0.0, 1.0, 100.0],
+        'dheight': [1.0, 100.0, 200.0],
+    })
+
+    output_df = remove_constant_columns(input_df)
+
+    assert target_df.equals(output_df)
