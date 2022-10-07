9805682ebbe9b1d16117ade8385c96fd43948513
AchilleSalaun <achillesalaun@gmail.com>
2022-10-07 14:03:36 +0100

    standardise is compliant with accessors

--- project/data/standardise.py
+++ project/data/standardise.py
@@ -1,57 +1,55 @@
 # Functions to standardise numerical data
 import re
 
-def get_standardisation(sdm):
+def get_standardisation(df):
     return {
-        "get_center" : lambda col: sdm.df[col].mean(),
-        "get_scale"  : lambda col: sdm.df[col].std()
+        "get_center" : lambda col: df[col].mean(),
+        "get_scale"  : lambda col: df[col].std()
     }
 
-def get_normalisation(sdm):
+def get_normalisation(df):
     return {
-        "get_center" : lambda col: sdm.df[col].min(),
-        "get_scale"  : lambda col: sdm.df[col].max() - sdm.df[col].min()
+        "get_center" : lambda col: df[col].min(),
+        "get_scale"  : lambda col: df[col].max() - df[col].min()
     }
 
 class Standardiser:
     def __init__(
             self,
-            dm,
+            df,
+            accessor_code,
+            ohe,
             get_center=lambda col: 0,
             get_scale =lambda col: 1,
             # standardise_target_duration=False
     ):
-        descriptor = dm.ohe.descriptor
-        separator  = dm.ohe.separator
+        descriptor = ohe.descriptor
+        separator  = ohe.separator
 
         self.m_columns_to_standardise = list()
-        for column in dm.features_list:  # dm.df.columns:
+        for column in getattr(df, accessor_code).features_list:
             if not re.findall(separator, column):
                 if descriptor.get_entry(column).is_numerical:
                     self.m_columns_to_standardise.append(column)
-
-        # if not standardise_target_duration:
-        #     self.m_columns_to_standardise = list(set(self.m_columns_to_standardise) - {sdm.duration_name})
 
         self.m_centers = {column: get_center(column) for column in self.m_columns_to_standardise}
         self.m_scales  = {column: get_scale(column)  for column in self.m_columns_to_standardise}
 
         assert 0 not in self.m_scales.values(), "One or more columns are constant, inducing a division by zero."
 
-    def __call__(self, dm):
+    def __call__(self, df):
         def _standardise_(s):
             return (s - self.m_centers[s.name]) / self.m_scales[s.name]
 
-        output_dm = dm.copy()
-        output_dm.df[self.m_columns_to_standardise] = output_dm.df[self.m_columns_to_standardise].apply(_standardise_)
+        output_df = df.copy()
+        output_df[self.m_columns_to_standardise] = output_df[self.m_columns_to_standardise].apply(_standardise_)
 
-        return output_dm
+        return output_df
 
-    def undo(self, dm):
+    def undo(self, df):
         def _undo_(s):
             return (s * self.m_scales[s.name]) + self.m_centers[s.name]
 
-        output_dm = dm.copy()
-        output_dm.df[self.m_columns_to_standardise] = output_dm.df[self.m_columns_to_standardise].apply(_undo_)
-        return output_dm
-
+        output_df = df.copy()
+        output_df[self.m_columns_to_standardise] = output_df[self.m_columns_to_standardise].apply(_undo_)
+        return output_df
--- tests/data/test_standardise.py
+++ tests/data/test_standardise.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 from project.data.standardise import *
 from project.data.describe    import Entry, Descriptor
-from project.data.datamanager import SurvivalDataManager
+from project.data.accessor    import build_survival_accessor
+
+_ = build_survival_accessor(event="c", duration="t", accessor_code="test_surv")
 
 DF = pd.DataFrame({
     "x1": [4., 2.],
@@ -38,47 +40,60 @@ class FakeOHE:
 
 
 OHE = FakeOHE(DESCRIPTOR)
-SDM = SurvivalDataManager(DF, event="c", duration="t", ohe=OHE)
 
 
 def test_nothing():
-    standardiser = Standardiser(SDM)
-    standardised_sdm = standardiser(SDM)
+    standardiser = Standardiser(
+        DF,
+        accessor_code="test_surv",
+        ohe=OHE
+    )
+    standardised_df = standardiser(DF)
 
-    assert standardised_sdm.equals(SDM)
+    assert standardised_df.equals(DF)
 
 def test_standardisation():
     s = np.sqrt(1 / 2)
-    sdm_ = SurvivalDataManager(pd.DataFrame(
+    df_ = pd.DataFrame(
         {
             "x1": [s  , -s ],
             "x2": [-s , s  ],
             "c" : [1  , 0  ],
             "t" : [10., 20.]
         }
-    ), event="c", duration="t")
+    )
 
-    standardiser = Standardiser(SDM, **get_standardisation(SDM))  # , standardise_target_duration=False)
-    standardised_sdm = standardiser(SDM)
+    standardiser = Standardiser(
+        DF,
+        "test_surv",
+        OHE,
+        **get_standardisation(DF)
+    )
+    standardised_df = standardiser(DF)
 
     decimals = 15  # floats are badly handled, so we can't use `equals`.
 
-    assert standardised_sdm.df.round(decimals).equals(sdm_.df.round(decimals)) \
-           and standardised_sdm.event_name    == sdm_.event_name               \
-           and standardised_sdm.duration_name == sdm_.duration_name
+    assert standardised_df.round(decimals).equals(df_.round(decimals))                                        \
+           and getattr(standardised_df, "test_surv").event    == getattr(df_, "test_surv").event    \
+           and getattr(standardised_df, "test_surv").duration == getattr(df_, "test_surv").duration
 
 
 def test_normalisation():
-    sdm_ = SurvivalDataManager(pd.DataFrame(
+    df_ = pd.DataFrame(
         {
             "x1": [1. , 0. ],
             "x2": [0. , 1. ],
             "c" : [1  , 0  ],
             "t" : [10., 20.]
         }
-    ), event="c", duration="t", ohe=OHE)
+    )
 
-    standardiser = Standardiser(SDM, **get_normalisation(SDM))  # , standardise_target_duration=False)
-    standardised_sdm = standardiser(SDM)
+    standardiser = Standardiser(
+        DF,
+        "test_surv",
+        OHE,
+        **get_normalisation(DF)
+    )
+    standardised_df = standardiser(DF)
 
-    assert standardised_sdm.equals(sdm_)
+    assert standardised_df.equals(df_)
