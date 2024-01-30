44e843ebb33cea4e91430a157c6b9b4726da76ee
AchilleSalaun <achillesalaun@gmail.com>
2024-01-30 11:42:37 +0000

    prototype

--- prototype/backend.py
+++ prototype/backend.py
@@ -1,33 +1,25 @@
 from flask import Flask, request
 import requests
 
+
 import dill  as pickle
+import numpy as np
 import pandas as pd
+import os
+import json
+import torch
+import re
 
-from lib.encode import OneHotEncoder
-from lib.describe import load_descriptor
-
-import sys
-sys.path += [
-    '',
-    '/home/engs2446/Softwares/miniconda3/envs/_flask_/lib/python311.zip',
-    '/home/engs2446/Softwares/miniconda3/envs/_flask_/lib/python3.11',
-    '/home/engs2446/Softwares/miniconda3/envs/_flask_/lib/python3.11/lib-dynload',
-    '/home/engs2446/Softwares/miniconda3/envs/_flask_/lib/python3.11/site-packages'
-]
-
-print()
-print(sys.path)
-print()
-print(sys.version)
-print()
-
+from xmlot.config    import Configurator
+from xmlot.data.load import load_descriptor
+from xmlot.misc.lists import difference, intersection, union
+from xmlot.misc.explanations import aggregate_shap_explanation, reformat_explanation
 app = Flask(__name__)
 
 # In terminal:
 """
-cd ~/Documents/Projects/Prototype/
-conda activate _flask_
+cd ~/Documents/Projects/Xamelot/Python/xmlot/prototype
+conda activate _xamelot_
 export FLASK_ENV=development
 export FLASK_APP=backend
 flask run --port 5001
@@ -37,86 +29,262 @@ flask run --port 5001
 # https://blog.miguelgrinberg.com/post/running-your-flask-application-over-https
 
 FRONTEND = "http://localhost:5000"
-CONFIG   = "/home/engs2446/Documents/Projects/Prototype/"
+MAIN_DIR = os.getcwd()
+YEARS = ['1', '5', '10']
+GRID_POINT = 20
 
-def _predict_denied_(offer):
-    # Load descriptor
-    descriptor = load_descriptor()  # description_230213
+#######################
+#    MISCELLANEOUS    #
+#######################
 
-    # Load model
-    file = open(CONFIG + "models/" + "deephit_denied" + ".pkl", 'rb')
-    model = pickle.load(file)
-    file.close()
 
-    # Load scaler
-    file = open(CONFIG + "scalers/" + "deephit_denied" + ".pkl", 'rb')
-    scaler = pickle.load(file)
-    file.close()
+class SHAPModel:
+    """
+    As SHAP models are not preserved during serialisation,
+    we need to rebuild a similar object that returns predictions as follows:
+    prediction = model.f(x)
+    """
 
-    # Embed offer
-    print(offer)
+    def __init__(self, model_, idx, accepted=True):
+        self.m_model = model_
+        self.m_idx   = idx
+        self.m_accepted = accepted
 
-    # embedded_offer, OHE_TRAIN = embed_data(
-    #     offer.copy(),
-    #     threshold=.90,
-    #     descriptor=DESCRIPTOR,
-    #     targets_to_focus=["dcens", "dsurv"],
-    #     visitor=TalkativeEmbedDataVisitor(),
-    #     **EPM
-    # )
+    def f(self, x):
+        if self.m_accepted:
+            return self.m_model.predict(x)[self.m_idx, :].detach().numpy()
+        else:
+            # Denied case
+            return self.m_model.predict_CIF(x).detach().numpy()[:, self.m_idx, :].transpose() # GRIDPOINT
 
-    df = pd.Dataframe(offer)
+def get_original_features(ohe):
+    return difference(ohe.columns, [
+        "dcens",
+        "dsurv",
+        "gcens",
+        "gsurv",
+        "gclass",
+        "pcens",
+        "psurv",
+        "pclass",
+        "tcens",
+        "tsurv",
+        "tclass",
+        "mclass"
+    ])
 
-    ohe = OneHotEncoder(
-        descriptor,
-        separator="#",
-        exceptions=list(),
-        default_categories={
-            'rhosp'         : 'T',
-            'dbg'           : 'O',
-            'rbg'           : 'O',
-            'dethnic'       : 'White',
-            'rethnic'       : 'White',
-            'prd'           : 'Unknown',
-            'dcod'          : 'Cerebrovascular',
-            'mgrade'        : 'Zero mismatches',
-            'hla_grp'       : 'Level 1',
-            'dial_type'     : 'Not on dialysis',
-            'alt_trend'     : '0.0',
-            'ast_trend'     : '0.0',
-            'amylase_trend' : '0.0',
-            'degfr_trend'   : '0.0'
+
+###########################
+#    PREDICTION ENGINE    #
+###########################
+
+
+class Engine:
+    def __init__(self, main_dir):
+        # Main directory
+        self.m_dir = main_dir
+        print("MAIN_DIR={}".format(self.m_dir))
+
+        # Features names and selection
+        with open(self.data_dir + "/features.json") as json_object:
+            self.m_features = json.load(json_object)
+
+        config = Configurator(desc_dir=main_dir)
+        self.m_descriptor = load_descriptor(config, csv_name="/data/description")
+
+    @property
+    def descriptor(self):
+        return self.m_descriptor
+
+    @property
+    def features(self):
+        return self.m_features
+
+    @property
+    def data_dir(self):
+        return self.m_dir + "/data"
+
+    def load(self, filename):
+        """
+        Load Python objects from serialised files.
+        """
+        file = open(self.data_dir + filename + ".pkl", 'rb')
+        unpickled = pickle.load(file)
+        file.close()
+        return unpickled
+
+    def format_result(self, predictions, explanations, ohe, scaler, i=None, scores=None):
+        pred = predictions if i is None else predictions[i]
+
+        formatted_explanations = [
+            reformat_explanation(aggregate_shap_explanation({
+                "base_values": explanation.base_values     if i is None else explanation.base_values[i],
+                "values"     : explanation.values.tolist() if i is None else explanation.values[:, i].tolist(),
+                "data"       : explanation.data.to_dict()
+            }),
+                ohe, scaler, self.descriptor) for explanation in explanations
+        ]
+
+        results = {
+            "predictions"  : pred,
+            "uncertainties": ["Uncertainty is not implemented yet." for _ in YEARS],  # TODO: Uncertainty quantification
+            "explanations" : formatted_explanations,
+            "scores"       : scores
         }
-    )
 
-    df = ohe.encode(df)
-    df = scaler(df)
+        return results
 
-    # Predict
-    prediction = model.predict_CIF(
-        df
-    )
+    def predict_accepted(self, offer, outcome):
+        def _build_score_(xcens_, xsurv_, offer_, prediction_, year_):
+            """
+                Compute L1 distance between prediction and ground-truth
+            """
+            if xcens_ in offer_.keys() and xsurv_ in offer_.keys():
+                if float(year_) * 365 < float(offer_[xsurv_][0]):  # Alive
+                    groundtruth = 0
+                elif offer_[xcens_][0] != "Censored":  # ........... Event
+                    groundtruth = 1
+                else:  # ........................................... Censored
+                    return "nan"
+                return np.abs(prediction_ - groundtruth)
+            # Else, if ground-truth is missing, do nothing.
 
-    print(prediction)
+        print("\t> PREDICT ACCEPTED ({})".format(outcome))
 
-    return prediction, "U"
+        # Load
+        ohe     = self.load("/dump/accepted/ohe")
+        scaler  = self.load("/dump/accepted/scaler")
+        # imputer = self.load("/dump/accepted/imputer") TODO: Handle missing values with imputation
+        model   = self.load("/dump/accepted/" + outcome + "/model")
 
-def _shap_():
-    return {
-        "dage" : 0,
-        "rage" : 0
-    }
+        # Embed offer
+        df = pd.DataFrame(offer)[intersection(offer.keys(), get_original_features(ohe))]
+        df = ohe.encode(df, reboot_encoded_columns=False)
+        df = scaler(df)
+
+        # Predict
+
+        prediction = model.predict(df).flatten().tolist()
+
+        # Explain
+        explainers   = self.load("/dump/accepted/" + outcome + "/explainers")
+        explanations = list()
+        for idx, explainer in enumerate(explainers):  # TODO: Explaining calibrated DeepHit is slow.
+            explainer.model = SHAPModel(model, idx=idx, accepted=True)
+            explanations.append(explainer(df.iloc[0]))
+
+        # Compare with ground-truth
+        scores = list()
+        for year in YEARS:
+            xcens, xsurv = ("gcens", "gsurv") if outcome == "graft" else ("pcens", "psurv")
+            scores.append(_build_score_(xcens, xsurv, offer, prediction, year))
+
+        return self.format_result(prediction, explanations, ohe, scaler, scores=scores)
+
+    def predict_denied(self, offer):
+        print("\t> PREDICT DENIED")
+
+        # Load
+        model       = self.load("/dump/denied/model")  # TODO: Calibration
+        ohe         = self.load("/dump/denied/ohe")
+        scaler      = self.load("/dump/denied/scaler")
+        # imputer = self.load("/dump/denied/imputer") TODO: Handle missing values with imputation
+        discretiser = self.load("/dump/denied/discretiser")
+
+        # Embed offer
+        df = pd.DataFrame(offer)[intersection(offer.keys(), get_original_features(ohe))]
+        df = ohe.encode(df, reboot_encoded_columns=False)
+        df = scaler(df)
+
+        # Derive Cumulative incidence function
+        predictions = model.predict(df).squeeze().tolist()
+
+        # Derive expected event times # TODO: might break with calibration
+        predicted_t = list()
+        t = torch.Tensor(discretiser.grid) / 365
+        pmf = model.predict_pmf(
+            df
+        )
+        for risk in range(3):
+            p = np.transpose(pmf[risk])[0]
+            predicted_t.append(torch.dot(t, p) / p.sum())
+
+        # Explain
+        explainer       = self.load("/dump/denied/explainer")
+        explainer.model = SHAPModel(model, idx=GRID_POINT, accepted=False)
+        explanations     = [explainer(df.iloc[0])]
+
+        results = {
+            "grid": discretiser.grid,
+            "outcomes": {
+                "Transplant": self.format_result(predictions, explanations, ohe, scaler, i=0),
+                "Removal from the waiting list": self.format_result(predictions, explanations, ohe, scaler, i=1),
+                "Death": self.format_result(predictions, explanations, ohe, scaler, i=2)
+            }
+        }
+
+        for i, outcome in enumerate(results["outcomes"].keys()):
+            results["outcomes"][outcome]["time"] = float(predicted_t[i])
+
+        # Ground-truth # TODO: to double check with real data
+        if "dcens" in offer.keys() and "dsurv" in offer.keys():
+            key_to_idx = {
+                "Transplant": 0,
+                "Removal": 1,
+                "Death": 2
+            }
+
+            i = np.argmin(list(map(lambda l: np.abs(offer["dsurv"][0]-l), discretiser.grid)))
+            if discretiser.grid[i] < offer["dsurv"][0]:
+                i = min(22, i+1)
+
+            xcens = key_to_idx[offer["dcens"][0]]
+            xsurv = offer["dsurv"][0]
+
+            scores = {
+                "error": float(np.abs(predicted_t[xcens] * 365 - xsurv)),
+                "likelihood": float(pmf[xcens][i][0] / np.transpose(pmf[xcens])[0].sum())
+            }
+
+            results["scores"] = scores
+
+        return results
+
+
+#######################
+#       BACKEND       #
+#######################
+
 
 @app.route('/predict', methods =['POST'])
 def predict():
     print("PREDICT")
+
+    engine  = Engine(main_dir=MAIN_DIR)
+
+    # Load feature list
+    features_list = union(engine.features["DENIED"], engine.features["ACCEPTED"])
+    features_list = sorted(list({re.match("^[^ #]*", feature)[0] for feature in features_list}))
+    print("\n> Feature list: {}\n".format(features_list))
+
+    # Load offer
     offer = request.json
-
+    offer = {k: offer[k] for k in features_list}
     print("\t<- {0}".format(offer))
+    results = {"offer": offer, "results": dict()}
 
-    results = dict()
-    results["prediction"], results["uncertainty"] = _predict_denied_(offer)
-    results["shap_values"] = _shap_()
+    offer   = {k: [v] for k, v in offer.items()}
+
+    # Accepted case
+    results["results"]["accepted"] = dict()
+    accepted_offer = {feature: offer[feature] for feature in engine.features["ACCEPTED"]}
+    results["results"]["accepted"]["graft"]   = engine.predict_accepted(accepted_offer, outcome="graft")
+    results["results"]["accepted"]["patient"] = engine.predict_accepted(accepted_offer, outcome="patient")
+
+    # Denied case
+    denied_offer = {feature: offer[feature] for feature in engine.features["DENIED"]}
+
+    results["results"]["denied"]   = engine.predict_denied(denied_offer)
 
     print("\t-> {0}".format(results))
 
--- prototype/frontend.py
+++ prototype/frontend.py
@@ -1,33 +1,213 @@
 from flask import Flask, request
+import json
+import os
 import requests
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import re
+import shap
+
+from xmlot.config     import Configurator
+from xmlot.data.load  import load_descriptor
+from xmlot.misc.lists import union, intersection
+from xmlot.misc.explanations import build_waterfall_plot
+
+import base64
+from io import BytesIO
 
 app = Flask(__name__)
 
 # In terminal:
 """
-cd ~/Documents/Projects/Prototype/
-conda activate _flask_
+cd ~/Documents/Projects/Xamelot/Python/xmlot/prototype
+conda activate _xamelot_
 export FLASK_ENV=development
 export FLASK_APP=frontend
 flask run --port 5000
 """
 
 BACKEND = "http://localhost:5001"
+MAIN_DIR = os.getcwd()
+YEARS = ['1', '5', '10']
+GRID_POINT = 20
+
+#######################
+#    MISCELLANEOUS    #
+#######################
+
+def _plot_to_html_(fig, style=""):
+    """
+        Turn a Matplotlib figure into HTML.
+    """
+    tmpfile = BytesIO()
+    fig.savefig(tmpfile, format='png', bbox_inches = 'tight' , dpi = 60)
+    output = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
+
+    return "<img src=\'data:image/png;base64,{0}\' style=\'margin:auto;display:block;{1}\'>".format(output, style)
+
+def _dict_to_html_table_(
+        d,
+        kprocessing= lambda k_: k_,
+        vprocessing= lambda v_: v_,
+        vertical_layout=False,
+        width=100
+):
+    """
+    Build a HTML table from a dictionary.
+
+    Args:
+        - d: the dictionary to process.
+        - kprocessing: a function that indicates how to display the keys.
+        - vprocessing: a function that indicates how to display the values.
+        - vertical_layouts: tells if key-values are displayed in columns or in rows.
+        - width: width of the table.
+
+    Returns: HTML
+    """
+
+    html_table = """
+<table style = "width:{0}%; margin:auto">""".format(width)
+
+    if vertical_layout:
+        for k, v in d.items():
+            html_table += """
+    <tr>
+        <th style="width:50%">{0}</th>
+        <td style="width:50%">{1}</td>
+    </tr> """.format(kprocessing(k), vprocessing(v))
+    else:
+        html_table += """
+    <tr>
+    """
+        for k in d.keys():
+            html_table += """
+        <th>{0}</th>""".format(kprocessing(k))
+        html_table += """
+    </tr>
+    <tr>"""
+        for v in d.values():
+            html_table += """
+        <td>{0}</td>""".format(vprocessing(v))
+        html_table += """
+    </tr>"""
+    html_table += """
+</table>"""
+
+    return html_table
+
+
+def _build_waterfall_plot_(explanation):
+    """
+    Build shap's waterfall plot from SHAP values,
+    and encode it for HTML.
+
+    Args:
+        - explanation: SHAP values, presented as a dictionary:
+            {
+                "values"      : SHAP values regarding each feature,
+                "base_values" : expected a priori outcomes,
+                "data"        : kidney offer
+            }
+    Returns: an encoded figure (str)
+    """
+    fig = build_waterfall_plot(explanation, max_display=10)
+
+    # Encode
+    return _plot_to_html_(fig, style="width:100%;")
+
+def _get_list_offers_():
+    """
+    List the files present in the offer directory.
+    No particular check is performed.
+    """
+    list_offers = sorted(os.listdir(MAIN_DIR + "/data/offers/"))
+    return list(map(lambda s: s[:-5], list_offers))
+
+
+######################
+#      FRONTEND      #
+######################
 
 
 @app.route('/')
 def main():
+    # Load
+    config = Configurator(desc_dir=MAIN_DIR)
+    descriptor = load_descriptor(config, csv_name="/data/description")
+
+    list_offers = _get_list_offers_()
+
+    offer_id = request.args.get("offer", default=None)
+    if offer_id not in list_offers:
+        offer_id = list_offers[0]
+    with open(MAIN_DIR + "/data/offers/{0}.json".format(offer_id)) as json_object:
+        json_offer = json.load(json_object)
+
+    # Initialisation
     html_page = """
-        <html>
-            <body>
-                <a href="http://localhost:5000/send">
-                    <button>
-                        Send offer
-                    </button>
-                </a>
-            </body>
-        </html>
-    """
+<html>
+    <style>
+        table, th, td {
+            border:1px solid black;
+        }
+    </style>
+    <body>"""
+
+    # Offer selection
+    html_page += """
+        <h1>Offer</h1>"""
+
+    html_page += """
+        <label for="offers">Select an offer:</label>
+        <select id="offers" name="offers" onchange="location = this.value;">"""
+    for id_ in list_offers:
+        selected = " selected" if id_ == offer_id else ""
+        html_page += """
+            <option value="/?offer={0}"{1}>{0}</option>""".format(id_, selected)
+    html_page += """
+        </select>"""
+
+    # Display offer
+    with open(MAIN_DIR + "/data/features.json") as json_object:
+        features = json.load(json_object)
+    features_list = union(features["ACCEPTED"], features["DENIED"])
+    features_list = sorted(list({re.match("^[^ #]*", feature)[0] for feature in features_list}))
+
+    offer = {k: json_offer[k] for k in features_list}
+
+    html_page += _dict_to_html_table_(
+        offer,
+        kprocessing     = lambda k_: descriptor.get_entry(k_).description,
+        vertical_layout = True,
+        width           = 60
+    )
+
+    # Ground-truth
+    targets_list = intersection(features["TARGETS"], json_offer.keys())
+    groundtruth = {k: json_offer[k] for k in targets_list}
+
+    if len(groundtruth) > 0:
+        html_page += _dict_to_html_table_(
+            groundtruth,
+            kprocessing     = lambda k_: descriptor.get_entry(k_).description,
+            vertical_layout = True,
+            width           = 60
+        )
+
+    # Send offer button
+    html_page += """
+        <div style="text-align:center">
+        <a href="http://localhost:5000/send?offer={0}">
+            <button>
+                Send offer
+            </button>
+        </a>
+        </div>
+    </body>
+</html>
+    """.format(offer_id)
 
     return html_page
 
@@ -35,33 +215,15 @@ def main():
 def send_offer():
     print("SEND_OFFER")
 
-    offer = {
-        "amm"                : 0.0,
-        "bmm"                : 1.0,
-        "crf_tx"             : 0.0,
-        "dage"               : 37,
-        "dcod"               : "Cerebrovascular",
-        "degfr_base"         : 107.620693,
-        "degfr_max"          : 107.620693,
-        "dial_type"          : "Not on dialysis",
-        "dpast_hypertension" : "Yes",
-        "dpast_smoker"       : "No",
-        "drmm"               : 0.0,
-        "dtype"              : "DBD",
-        "hla_grp"            : "Level 2",
-        "matchbty"           : 5.0,
-        "mgrade"             : "Favourable mismatch",
-        "offer_wait"         : 389.0,
-        "prd"                : "Unknown",
-        "r_a_hom"            : "Heterozygous",
-        "r_dr_hom"           : "Heterozygous",
-        "rage"               : 45,
-        "rbg"                : "O",
-        "rcmv"               : "Positive",
-        "rethnic"            : "White",
-        "rhosp"              : "N",
-        "rsex"               : "Male"
-    }
+    list_offers = _get_list_offers_()
+
+    offer_id = request.args.get("offer", default=None)
+
+    print("<{0}>".format(offer_id))
+
+    assert offer_id in list_offers, "The offer id must refer to a valid offer."
+    with open(MAIN_DIR + "/data/offers/{0}.json".format(offer_id)) as json_object:
+        offer = json.load(json_object)
 
     print("\t-> {0}".format(offer))
 
@@ -69,41 +231,206 @@ def send_offer():
 
 @app.route('/results', methods=['POST'])
 def display_results():
-    print("RESULTS ")
+    print("\nDISPLAYING RESULTS\n")
+
+    # Load
+    config = Configurator(desc_dir=MAIN_DIR)
+    descriptor = load_descriptor(config, csv_name="/data/description")
 
     results = request.json
+    offer   = results["offer"]
 
     print("\t<- {0}".format(results))
 
-    html = """
+    # Initialisation of the HTML page
+    html_page = """
 <html>
     <style>
-    table, th, td {{
+    table, th, td {
     border:1px solid black;
-    }}
+    }
     </style>
     <body>
-        <table>
+    <div style="width:80%; margin:auto">"""
+
+    # Display offer
+    html_page += """
+        <h1>Offer</h1>
+"""
+    html_page += _dict_to_html_table_(
+        offer,
+        kprocessing     = lambda k_: descriptor.get_entry(k_).description,
+        vertical_layout = True,
+        width           = 70
+    )
+
+    # Display results
+    html_page += """
+        <h1>If the offer is accepted:</h1>"""
+
+    print("\n\t> ACCEPTED:")
+
+    results_graft   = results["results"]["accepted"]["graft"]
+    results_patient = results["results"]["accepted"]["patient"]
+
+    for year_idx, year in enumerate(YEARS):
+        print("----- {} -----".format(year))
+        html_page += """
+        <h2>Before year {0}</h2>""".format(year)
+
+        html_page += """
+        <table style = "width:100%; margin:auto; table-layout:fixed">
             <tr>
-                <th>Prediction</th>
-                <th>{0}</th>
+                <th>Graft failure</th>
+                <th>Patient death</th>
             </tr>
             <tr>
-                <th>Uncertainty</th>
+                <th>{0}</th>
                 <th>{1}</th>
             </tr>
-            <tr>
-                <th>SHAP</th>
                 <th>{2}</th>
+                <th>{3}</th>
             </tr>
-        </table>
+        </table>""".format(
+            "{0:.2%} ({1})".format(
+                results_graft["predictions"][year_idx],
+                results_graft["uncertainties"][year_idx]
+            ),
+            "{0:.2%} ({1})".format(
+                results_patient["predictions"][year_idx],
+                results_patient["uncertainties"][year_idx]
+            ),
+            _build_waterfall_plot_(results_graft["explanations"][year_idx]),
+            _build_waterfall_plot_(results_patient["explanations"][year_idx])
+        )
+
+        # Print validation score if possible
+        if "score" in results_graft.keys() and "score" in results_patient.keys():
+            html_page += """
+            <div style="margin-top:1em">
+            Validation:
+            <ul>"""
+
+            score = results_graft["score"]
+            if 365 * float(year) < float(offer["gsurv"]):
+                status = "functioning"
+            else:
+                if offer["gcens"] == "Graft failure":
+                    status = "failed"
+                else:
+                    status = "unknown"
+            html_page += """
+                <li>
+                    {0}: at year {2}, the graft is {3}.
+                    According to the model, the likelihood of such status is {1}
+                    (the closer to 100% the better).
+                </li>""".format(
+                "Graft",
+                "{0:.2%}".format(1 - score) if type(score) is float else "nan",
+                year,
+                status
+            )
+
+            score = results_patient["score"]
+            if 365 * float(year) < float(offer["psurv"]):
+                status = "alive"
+            else:
+                if offer["pcens"] == "Death of recipient":
+                    status = "dead"
+                else:
+                    status = "unknown"
+            html_page += """
+                <li>
+                    {0}: at year {2}, the patient is {3}.
+                    According to the model, the likelihood of such status is {1}
+                    (the closer to 100% the better).
+                </li>""".format(
+                "Patient",
+                "{0:.2%}".format(1 - score) if type(score) is float else "nan",
+                year,
+                status
+            )
+
+            html_page += """
+            </ul>
+            </div>"""
+
+    html_page += """
+        <h1>If the offer is instead declined:</h1>"""
+
+    print("\n\t> DENIED")
+
+    fig, ax = plt.subplots(figsize=(20, 10))
+    grid = results["results"]["denied"]["grid"]
+    for i, (outcome, predictions) in enumerate(results["results"]["denied"]["outcomes"].items()):
+
+        print(predictions)
+
+        x      = list(map(lambda x_: x_ / 365, grid))
+        y      = predictions["predictions"]
+        x_pred = predictions["time"]
+
+        html_page += """
+        Expected event time if '{0}' occurs first: {1:.1f} years  ({2:.1f} days).<br>""".format(
+            outcome,
+            x_pred,
+            x_pred * 365
+        )
+
+        x[-1] = 5
+        plt.step(x, y, label="{0}".format(outcome), color="C{0}".format(i))
+
+        if x_pred <= x[-1]:
+            plt.vlines(x_pred, 0, 1, color="C{0}".format(i), linestyles="dashed")
+
+    plt.legend()
+
+    html_page += _plot_to_html_(fig)
+
+    html_page += "Explanations at {0:.1f} years:".format(grid[GRID_POINT] / 365)
+
+    html_page += """
+    <div style="overflow:hidden">"""
+
+    results_denied = results["results"]["denied"]["outcomes"]
+    width = int((1 / len(results_denied)) * 100)
+    for outcome in results_denied.keys():
+        html_page += """
+        <div style="width:{}%;float:left">""".format(width)
+
+        html_page += _build_waterfall_plot_(results_denied[outcome]["explanations"][0])
+        html_page += """
+        <div style="text-align:center">{}</div>""".format(outcome)
+
+        html_page += """
+        </div>"""
+
+    html_page += """
+    </div>"""
+
+    if "scores" in results["results"]["denied"].keys():
+        error = results["results"]["denied"]["scores"]["error"]
+        likelihood = float(results["results"]["denied"]["scores"]["likelihood"])
+
+        html_page += """
+        <div style="margin-top:1em">
+        Validation:
+        <ul>
+            <li>'{0}' occured after waiting {1} days: 
+            according to the model, the likelihood of such event is {2:.2%}.</li>
+            <li>Assuming '{0}' occured first, 
+            the error between the predicted event time and the actual one is {3:.0f} days.</li>
+        </ul>
+        </div>
+        """.format(offer["dcens"], offer["dsurv"], likelihood, error)
+
+    html_page += """
+        <div style="text-align:center">
+            <a href="http://localhost:5000/">Return to offer selection.</a>
+        </div>
+    </div>
     </body>
 </html>
-    """.format(
-        results["prediction"],
-        results["uncertainty"],
-        results["shap_values"]
-    )
-    return html
+    """
 
-
+    return html_page
